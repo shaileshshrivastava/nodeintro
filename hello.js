45c9a0760c9bfbbd8e6da2aee50713d74f793a90
Shailesh Shrivastava <shailesh.shrivastava@gmail.com>
2020-03-15 11:41:24 -0700

    updated package.json, proc file, and hello.js PORT

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require ('url');
+const PORT = process.env.PORT || 5000
 
 http.createServer(function(req,res){
 	var q = url.parse (req.url, true);
@@ -17,6 +18,5 @@ http.createServer(function(req,res){
 		res.end();
 		return res.end();
 	})
-}).listen(8080);
-
+}).listen(PORT);
 console.log("Server Listening on Port 8080");
